3d4c785339ca8da97159ac5a31abc9854092a193
Charles Moulliard <cmoulliard@redhat.com>
2022-04-19 22:40:43 +0200

    Export to separate subfolders: root/java/server and export the CA cert too to the pk12 store

--- scripts/gen-ca-selfsign-import.sh
+++ scripts/gen-ca-selfsign-import.sh
@@ -16,34 +16,6 @@ HOSTNAME=${HOSTNAME:=127.0.0.1}
 FQ_HOSTNAME=${HOSTNAME}.nip.io
 TEMP_DIR="_temp"
 
-create_openssl_cfg() {
-CFG=$(cat <<EOF
-[req]
-distinguished_name = subject
-x509_extensions    = x509_ext
-prompt             = no
-[subject]
-C  = BE
-ST = Namur
-L  = Florennes
-O  = Red Hat
-OU = Snowdrop
-CN = "$FQ_HOSTNAME"
-[x509_ext]
-basicConstraints        = critical, CA:TRUE
-subjectKeyIdentifier    = hash
-authorityKeyIdentifier  = keyid:always, issuer:always
-keyUsage                = critical, cRLSign, digitalSignature, keyCertSign
-nsComment               = "OpenSSL Generated Certificate"
-subjectAltName          = @alt_names
-[alt_names]
-DNS.1 = "$FQ_HOSTNAME"
-EOF
-)
-echo "$CFG"
-}
-
-
 # Defining some colors for output
 RED='\033[0;31m'
 NC='\033[0m' # No Color
@@ -78,22 +50,22 @@ log() {
 
 log_line "CYAN" "The fully qualified name of the server is: ${FQ_HOSTNAME}"
 
-rm -rf ${TEMP_DIR} && mkdir -p ${TEMP_DIR}
+rm -rf ${TEMP_DIR} && mkdir -p ${TEMP_DIR}/{root,server,java}
 
 log_line "CYAN" "Generate the CA certificate (ca.pem) and its private key file (ca.key)"
-openssl genrsa -out ${TEMP_DIR}/ca.key 2048
+openssl genrsa -out ${TEMP_DIR}/root/ca.key 2048
 openssl req -x509 -new \
     -nodes \
     -sha256 \
     -days 3650 \
     -subj '/CN=CA Authory/O=Red Hat/L=Florennes/C=BE' \
-    -key ${TEMP_DIR}/ca.key \
-    -out ${TEMP_DIR}/ca.pem
+    -key ${TEMP_DIR}/root/ca.key \
+    -out ${TEMP_DIR}/root/ca.pem
 
 log_line "CYAN" "Generate the server or tls key (tls.pem) & certificate signing request (tls.csr)"
 
 #Create OpenSSL config
-cat <<EOF > ${TEMP_DIR}/ca.cnf
+cat <<EOF > ${TEMP_DIR}/root/ca.cnf
 [req]
 default_bits = 4096
 prompt = no
@@ -109,7 +81,7 @@ OU = Snowdrop
 CN = "$FQ_HOSTNAME"
 EOF
 
-cat <<EOF > ${TEMP_DIR}/v3.ext
+cat <<EOF > ${TEMP_DIR}/root/v3.ext
 basicConstraints        = CA:FALSE
 subjectKeyIdentifier    = hash
 authorityKeyIdentifier  = keyid,issuer
@@ -123,24 +95,32 @@ DNS.1 = ${FQ_HOSTNAME}
 EOF
 
 openssl req -new -sha256 -nodes \
-    -out ${TEMP_DIR}/tls.csr \
+    -out ${TEMP_DIR}/server/tls.csr \
     -newkey rsa:4096 \
-    -keyout ${TEMP_DIR}/tls.key -config <( cat ${TEMP_DIR}/ca.cnf )
+    -keyout ${TEMP_DIR}/server/tls.key -config <( cat ${TEMP_DIR}/root/ca.cnf )
 
 log_line "CYAN" "Sign the Server or TLS CSR with CA and generates the certificate file (tls.pem)"
 openssl x509 -req \
-    -in ${TEMP_DIR}/tls.csr \
-    -CA ${TEMP_DIR}/ca.pem \
-    -CAkey ${TEMP_DIR}/ca.key -CAcreateserial \
-    -out ${TEMP_DIR}/tls.pem -days 3650 -sha256 \
-    -extfile ${TEMP_DIR}/v3.ext
+    -in ${TEMP_DIR}/server/tls.csr \
+    -CA ${TEMP_DIR}/root/ca.pem \
+    -CAkey ${TEMP_DIR}/root/ca.key -CAcreateserial \
+    -out ${TEMP_DIR}/server/tls.pem -days 3650 -sha256 \
+    -extfile ${TEMP_DIR}/root/v3.ext
 
 log_line "CYAN" "Combine the server or TLS key and certificate in a PKCS#12 (P12) bundle"
-openssl pkcs12 -inkey ${TEMP_DIR}/tls.key -in ${TEMP_DIR}/tls.pem -passin pass:password -passout pass:password -export -out ${TEMP_DIR}/tls.p12
+openssl pkcs12 -inkey ${TEMP_DIR}/server/tls.key -in ${TEMP_DIR}/server/tls.pem -CAfile ${TEMP_DIR}/root/ca.pem -chain -passin pass:password -passout pass:password -export -out ${TEMP_DIR}/server/tls.p12
 
 log_line "CYAN" "Generate the jks file from the p12 file"
-keytool -importkeystore -srckeystore ${TEMP_DIR}/tls.p12 -srcstoretype pkcs12 -srcstorepass password -deststorepass password -destkeystore ${TEMP_DIR}/tls.jks
+keytool -importkeystore -srckeystore ${TEMP_DIR}/server/tls.p12 -srcstoretype pkcs12 -srcstorepass password -deststorepass password -destkeystore ${TEMP_DIR}/java/tls.jks
 
 log_line "CYAN" "Check the content of the jks store"
-keytool -list -keystore ${TEMP_DIR}/tls.jks -storepass password
+keytool -list -keystore ${TEMP_DIR}/java/tls.jks -storepass password
+
+log_line "CYAN" "Exporting the client and CA certificates"
+openssl pkcs12 -in ${TEMP_DIR}/server/tls.p12 -passin pass:password -passout pass:password -clcerts -nokeys | openssl x509 -out ${TEMP_DIR}/server/tls.crt
+openssl pkcs12 -in ${TEMP_DIR}/server/tls.p12 -passin pass:password -passout pass:password -cacerts -nokeys | openssl x509 -out ${TEMP_DIR}/server/ca.crt
+
+log_line "CYAN" "Exporting the public key"
+openssl x509 -pubkey -in ${TEMP_DIR}/server/tls.crt -noout > ${TEMP_DIR}/server/tls_pub.key
+
 
