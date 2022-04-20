b990c5bcde2d2ebd9c2748369a0ecacf97853206
Charles Moulliard <cmoulliard@redhat.com>
2022-04-20 10:19:34 +0200

    Pass as arg the name of the certificate to parse/read

--- scripts/read_cert.sh
+++ scripts/read_cert.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-echo "Ca certificate"
-kubectl get secret/snowdrop-p12 -n demo -o json | jq -r '.data."ca.crt"' | base64 -d -
+CERT_NAME=${1}
 
-echo "TLS certificate"
-kubectl get secret/snowdrop-p12 -n demo -o json | jq -r '.data."tls.crt"' | base64 -d -
+if [ -z "$1" ]
+  then
+    echo "No certificate file name to parsed passed ! "
+    echo "Usage: $0 [Certificate file name]"
+	  exit 0
+fi
+
+echo "Name of file certificate to parse: "
+kubectl get secret/snowdrop-p12 -n demo -o json | jq -r .data.\"${CERT_NAME}\" | base64 -d - | openssl x509 -noout -text
