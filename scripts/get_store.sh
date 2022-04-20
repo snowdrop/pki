83dd922c1376df0b13f1a4b37e27a2cff1d77b6b
Charles Moulliard <cmoulliard@redhat.com>
2022-04-20 14:09:32 +0200

    Loop through a list of items

--- scripts/get_store.sh
+++ scripts/get_store.sh
@@ -15,8 +15,12 @@ mkdir -p ${TEMP_DIR}/cert-manager
 echo "================================================"
 echo "Got the trust and keystore p12 files from the tls-secret"
 echo "================================================"
-kubectl get secret/tls-secret -n demo -o json | jq -r '.data."keystore.p12"' | base64 -d - > ${TEMP_DIR}/cert-manager/keystore.p12
-kubectl get secret/tls-secret -n demo -o json | jq -r '.data."truststore.p12"' | base64 -d - > ${TEMP_DIR}/cert-manager/truststore.p12
 
-openssl pkcs12 -info -in ${TEMP_DIR}/cert-manager/keystore.p12 -passin pass:${PASSWORD} -passout pass:${PASSWORD}
-openssl pkcs12 -info -in ${TEMP_DIR}/cert-manager/truststore.p12 -passin pass:${PASSWORD} -passout pass:${PASSWORD}
+for VAL in "keystore" "truststore"; do
+  kubectl get secret/tls-secret -n demo -o json | jq -r .data.\"${VAL}.p12\" | base64 -d - > ${TEMP_DIR}/cert-manager/${VAL}.p12
+  echo "================================================"
+  echo "Reading the $VAL.p12"
+  echo "================================================"
+  openssl pkcs12 -info -in ${TEMP_DIR}/cert-manager/${VAL}.p12 -passin pass:${PASSWORD} -passout pass:${PASSWORD}
+done
+
