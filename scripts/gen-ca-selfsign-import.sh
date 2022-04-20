383a05087e6ef5e23cfd6a78ede3718543506e90
Charles Moulliard <cmoulliard@redhat.com>
2022-04-20 07:55:20 +0200

    Remove the commands to export which are not needed as they exist

--- scripts/gen-ca-selfsign-import.sh
+++ scripts/gen-ca-selfsign-import.sh
@@ -62,7 +62,7 @@ openssl req -x509 -new \
     -key ${TEMP_DIR}/root/ca.key \
     -out ${TEMP_DIR}/root/ca.pem
 
-log_line "CYAN" "Generate the server or tls key (tls.pem) & certificate signing request (tls.csr)"
+log_line "CYAN" "Generate the server or tls key (tls.key) & certificate signing request (tls.csr)"
 
 #Create OpenSSL config
 cat <<EOF > ${TEMP_DIR}/root/ca.cnf
@@ -99,26 +99,22 @@ openssl req -new -sha256 -nodes \
     -newkey rsa:4096 \
     -keyout ${TEMP_DIR}/server/tls.key -config <( cat ${TEMP_DIR}/root/ca.cnf )
 
-log_line "CYAN" "Sign the Server or TLS CSR with CA and generates the certificate file (tls.pem)"
+log_line "CYAN" "Sign the Server or TLS CSR with CA and generates the certificate file (tls.crt)"
 openssl x509 -req \
     -in ${TEMP_DIR}/server/tls.csr \
     -CA ${TEMP_DIR}/root/ca.pem \
     -CAkey ${TEMP_DIR}/root/ca.key -CAcreateserial \
-    -out ${TEMP_DIR}/server/tls.pem -days 3650 -sha256 \
+    -out ${TEMP_DIR}/server/tls.crt -days 3650 -sha256 \
     -extfile ${TEMP_DIR}/root/v3.ext
 
 log_line "CYAN" "Combine the server or TLS key and certificate in a PKCS#12 (P12) bundle"
-openssl pkcs12 -inkey ${TEMP_DIR}/server/tls.key -in ${TEMP_DIR}/server/tls.pem -CAfile ${TEMP_DIR}/root/ca.pem -chain -passin pass:password -passout pass:password -export -out ${TEMP_DIR}/server/tls.p12
+openssl pkcs12 -inkey ${TEMP_DIR}/server/tls.key -in ${TEMP_DIR}/server/tls.crt -CAfile ${TEMP_DIR}/root/ca.pem -chain -passin pass:password -passout pass:password -export -out ${TEMP_DIR}/server/tls.p12
 
 log_line "CYAN" "Generate the jks file from the p12 file"
 keytool -importkeystore -srckeystore ${TEMP_DIR}/server/tls.p12 -srcstoretype pkcs12 -srcstorepass password -deststorepass password -destkeystore ${TEMP_DIR}/java/tls.jks
 
 log_line "CYAN" "Check the content of the jks store"
 keytool -list -keystore ${TEMP_DIR}/java/tls.jks -storepass password
-
-log_line "CYAN" "Exporting the client and CA certificates"
-openssl pkcs12 -in ${TEMP_DIR}/server/tls.p12 -passin pass:password -passout pass:password -clcerts -nokeys | openssl x509 -out ${TEMP_DIR}/server/tls.crt
-openssl pkcs12 -in ${TEMP_DIR}/server/tls.p12 -passin pass:password -passout pass:password -cacerts -nokeys | openssl x509 -out ${TEMP_DIR}/server/ca.crt
 
 log_line "CYAN" "Exporting the public key"
 openssl x509 -pubkey -in ${TEMP_DIR}/server/tls.crt -noout > ${TEMP_DIR}/server/tls_pub.key
