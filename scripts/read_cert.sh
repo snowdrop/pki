d4745f61ed88efe07a5e8f46c53008e9e5c32a70
Charles Moulliard <cmoulliard@redhat.com>
2022-04-20 10:21:17 +0200

    Highlight the header

--- scripts/read_cert.sh
+++ scripts/read_cert.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-CERT_NAME=${1}
-
 if [ -z "$1" ]
   then
     echo "No certificate file name to parsed passed ! "
@@ -9,5 +7,9 @@ if [ -z "$1" ]
 	  exit 0
 fi
 
-echo "Name of file certificate to parse: "
+CERT_NAME=${1}
+
+echo "================================================"
+echo "Name of file certificate to parse: ${CERT_NAME}"
+echo "================================================"
 kubectl get secret/snowdrop-p12 -n demo -o json | jq -r .data.\"${CERT_NAME}\" | base64 -d - | openssl x509 -noout -text
